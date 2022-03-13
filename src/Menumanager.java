8229042f2cd8a49ceaafaf1fa7df0ca5156ee031
square1997 <76528629+square1997@users.noreply.github.com>
2022-03-13 21:12:03 +0900

    added Manumanager.java

--- src/Menumanager.java
+++ src/Menumanager.java
@@ -2,7 +2,12 @@
 public class Menumanager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("1. Add Pay");
+		System.out.println("2. Delete Pay");
+		System.out.println("3. Edit Pay");
+		System.out.println("4. View Pay");
+		System.out.println("5. Show a menu");
+		System.out.println("6. Exit");
 
 	}
 
