8d9515672becd2b5f96886a4efeadb8060d7a298
square1997 <76528629+square1997@users.noreply.github.com>
2022-03-15 22:40:22 +0900

    updated Menumanager.java

--- src/Manumanager.java
+++ src/Manumanager.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
 
 public class Manumanager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Pay");
-		System.out.println("2. Delete Pay");
-		System.out.println("3. Edit Pay");
-		System.out.println("4. View Pay");
-		System.out.println("5. Show a menu");
-		System.out.println("6. Exit");
+		int selectedMenuNum = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while (selectedMenuNum != 6) {
+				System.out.println("1. Add Pay");
+				System.out.println("2. Delete Pay");
+				System.out.println("3. Edit Pay");
+				System.out.println("4. View Pay");
+				System.out.println("5. Show a menu");
+				System.out.println("6. Exit");
+				System.out.print("Select Menu between 1-6 : ");
+				selectedMenuNum = input.nextInt();
+				switch(selectedMenuNum) {
+				case 1 :
+				case 2 :
+				case 3 :
+				case 4 :
+				case 5 :
+					System.out.println();
+					continue;
+				case 6 :
+					System.out.println("End the program.");
+					break;
+				}
+			}
+		
+		}
 
 	}
 
-}
+
